2a39d5de474257ee9e5fe9c77f92ab0ac829e00f
osmergit <osmer1976@gmail.com>
2024-07-30 11:07:33 -0500

    Segundo commit

--- archivo.js
+++ archivo.js
@@ -16,9 +16,58 @@ document.querySelector("#boton1").addEventListener("click", () => {
     document.querySelector("#boton3").style.display = "block"; 
 });
 
+document.querySelector("#boton2").addEventListener("click", () => {
+    // Captura el contenido del área de texto
+    const contenidoTextarea = document.querySelector("#texto-izquierdo").value;
+    // Llama a la función copiar con el contenido capturado
+    desencriptar('p', contenidoTextarea);
+    document.querySelector("#mi-imagen").style.display = "none";
+    document.querySelector("#boton3").style.display = "block"; 
+});
+
 function copiar(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
+  
+        // Reemplazar las letras según las reglas
+        const encriptado = texto
+            .replace(/e/g, 'enter')
+            .replace(/i/g, 'imes')
+            .replace(/a/g, 'ai')
+            .replace(/o/g, 'ober')
+            .replace(/u/g, 'ufat');
+        
+        elementoHTML.innerHTML = encriptado;
+        return encriptado;
+    
 }
 
-console.log("PAso por aca");
+function desencriptar(elemento, texto){
+    alert("paso por aca");
+    let elementoHTML = document.querySelector(elemento);
+    //let elementoHTML = document.querySelector("#texto-izquierdo").value;
+    //const desencriptado = elementoHTML
+    const desencriptado = texto
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    elementoHTML.innerHTML = desencriptado;
+console.log(desencriptado);
+return desencriptado;
+}
+
+function copiarTexto(){
+
+    const texto = document.getElementById("parrafo");
+    const tmp = document.createElement("input");
+    tmp.value = texto.textContent;
+    document.body.appendChild(tmp);
+
+    tmp.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(tmp);
+    alert("Texto Copiado:" + texto.textContent);
+}
+
